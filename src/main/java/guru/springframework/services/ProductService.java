fe27595933519d8e11fd34a8fe7fcb1a92e4fe48
Juan David Sánchez <juancho85@gmail.com>
2017-01-13 19:47:31 +0100

    added Product Model, Controller, service and template

---
+++ src/main/java/guru/springframework/services/ProductService.java
@@ -0,0 +1,14 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by juancho on 13/01/2017.
+ */
+public interface ProductService {
+
+    List<Product> listAllProducts();
+}
+
